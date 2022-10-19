75ca146d3eb23e62f184d1dc63e94cbfb369b033
salman9653 <salman9915189734@gmail.com>
2022-10-19 22:56:35 +0530

    added song details page

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -15,7 +15,7 @@ const TopChartCard = ({ song, i, isPlaying, activeSong, handlePauseClick, handle
   <div className={`w-full flex flex-row items-center hover:bg-[#4c426e] ${activeSong?.title === song?.title ? 'bg-[#4c426e]' : 'bg-transparent'} py-2 px-4 rounded-lg cursor-pointer mb-2`}>
     <h3 className="font-bold text-base text-white mr-3">{i + 1}.</h3>
     <div className="flex-1 flex flex-row justify-between items-center">
-      <img className="w-16 h-16 rounded-lg" src={song?.images?.coverart} alt={song?.title} />
+      <img className="w-16 h-16 rounded-full" src={song?.images?.coverart} alt={song?.title} />
       <div className="flex-1 flex flex-col justify-center mx-3">
         <Link to={`/songs/${song.key}`}>
           <p className="text-xl font-bold text-white">
