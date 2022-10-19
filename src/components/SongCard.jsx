b900b092b2c9a308cbf466b6eb152d2113467fab
salman9653 <salman9915189734@gmail.com>
2022-10-19 12:16:32 +0530

    added side bar (navbar)

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -6,13 +6,20 @@ import { playPause, setActiveSong } from "../redux/features/playerSlice";
 
 const SongCard = ({ song, i, isPlaying, activeSong, data }) => {
 
-  const handlePauseClick = () => { }
-  const handlePlayClick = () => { }
+  const dispatch = useDispatch();
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  }
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  }
 
 
   return (
-    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
-      <div className="relative w-full h-56 group">
+    <div className="flex flex-col w-[200px] p-2 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
+      <div className="relative w-full h-50 group">
         <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 rounded-lg group-hover:flex
       ${activeSong?.title === song.title ? 'flex bg-black bg-opacity-70' : 'hidden'}`}>
           <PlayPause
