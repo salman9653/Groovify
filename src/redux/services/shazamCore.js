75ac4a95804e905693e252f7aa683787ecfdeb4a
salman9653 <salman9915189734@gmail.com>
2022-10-21 18:16:49 +0530

    added related songs component

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -12,9 +12,9 @@ export const shazamCoreApi = createApi({
     endpoints: (builder) => ({
         getTopCharts: builder.query({ query: () => '/charts/world' }),
         // getSongsByGenre: builder.query({ query: (genre) => `/charts/genre-world?genre_code=${genre}` }),
-        // getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
+        getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
         // getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
-        // getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
+        getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
         getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
         getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
     })
@@ -23,9 +23,9 @@ export const shazamCoreApi = createApi({
 export const {
     useGetTopChartsQuery,
     // // useGetSongsByGenreQuery,
-    // useGetSongsByCountryQuery,
+    useGetSongsByCountryQuery,
     // useGetSongsBySearchQuery,
-    // useGetArtistDetailsQuery,
+    useGetArtistDetailsQuery,
     useGetSongDetailsQuery,
     useGetSongRelatedQuery,
 } = shazamCoreApi;
