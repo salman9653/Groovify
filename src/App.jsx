a69d4b04229478432ca00225e283294ce5f5fb40
salman9653 <salman9915189734@gmail.com>
2022-10-21 19:58:01 +0530

    minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ const App = () => {
       </div>
 
       {activeSong?.title && (
-        <div className="absolute h-28 bottom-0 left-0 right-0 flex animate-slideup bg-gradient-to-br from-white/10 to-[#2a2a80] backdrop-blur-lg rounded-t-3xl z-10">
+        <div className="absolute h-28 mx-10 bottom-3 left-0 right-0 flex animate-slideup bg-gradient-to-br from-white/10 to-[#2a2a80] backdrop-blur-lg rounded-3xl z-10">
           <MusicPlayer />
         </div>
       )}
